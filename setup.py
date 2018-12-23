49b734f3494f9367a9ba92238f1c49f23208d56e
Bryan Blunt <bryan@blunt.me.uk>
2018-12-23 12:17:43 +0000

    Version to 0.0.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lightwave2",
-    version="0.0.9",
+    version="0.0.10",
     author="Bryan Blunt",
     author_email="bryan@blunt.me.uk",
     description="Controls for Lightwave RF second generation devices",
