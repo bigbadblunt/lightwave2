f63431d09d5982177bcadad4167c7ea97b031bfb
Bryan Blunt <bryan@blunt.me.uk>
2021-12-19 18:48:00 +0000

    update login code

--- lightwave2/lightwave2.py
+++ lightwave2/lightwave2.py
@@ -337,9 +337,9 @@ class LWLink2:
     #########################################################
 
     async def async_connect(self, max_tries=5, _retry=0):
-        try:
+        #try:
             return await self._connect_to_server()
-        except Exception as exp:
+        #except Exception as exp:
             if (max_tries == 0) or (_retry < max_tries):
                 retry_delay = min(2 ** (_retry + 1), 120)
                 _LOGGER.warning("async_connect: Cannot connect (exception '{}'). Waiting {} seconds to retry".format(repr(exp), retry_delay))
@@ -395,11 +395,11 @@ class LWLink2:
         _LOGGER.debug("get_access_token_username: Requesting authentication token (using username and password)")
         authentication = {"email": self._username, "password": self._password, "version": VERSION}
         async with self._session.post(AUTH_SERVER, headers={"x-lwrf-appid": "ios-01"}, json=authentication) as req:
+            _LOGGER.debug("get_access_token_username: Received response: status {}".format(req.status))
             if req.status == 200:
                 _LOGGER.debug("get_access_token_username: Received response: [contents hidden for security]")
                 #_LOGGER.debug("get_access_token_username: Received response: {}".format(await req.json()))
                 self._authtoken = (await req.json())["tokens"]["access_token"]
-
 
     # TODO check for token expiry
     async def _get_access_token_api(self):
